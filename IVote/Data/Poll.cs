20ff9fa0039ba439a85e9db4e9c39bdb535d60eb
EmreCop <99261888+EmreCop@users.noreply.github.com>
2024-01-18 13:40:27 +0100

    Added datebase poll and some bug fixes

--- IVote/Data/Poll.cs
+++ IVote/Data/Poll.cs
@@ -3,18 +3,27 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace IVote.Data
 {
-	public class Poll
-	{
-		[Key]
-		public int Id { get; set; }
-		public string? Naam { get; set; }
-		public required DateOnly StartDate {get; set;}
-		public required DateOnly EndDate { get; set; }
+  public class Poll
+  {
+    [Key]
+    public int Id { get; set; }
+    public string? Naam { get; set; }
+    public required DateOnly StartDate { get; set; }
+    public required DateOnly EndDate { get; set; }
 
-		[ForeignKey("Kandidaat ")]
-		public virtual List<int>? KanditaatIds { get; set; }
-		public int? TotalAmoutofVotes { get; set; }
-		
-		public int WinaarPollID { get; set; }
-	}
+    [ForeignKey("Kandidaat ")]
+    public virtual List<int>? KanditaatIds { get; set; }
+    public int? TotalAmoutofVotes { get; set; }
+
+    public int WinaarPollID { get; set; }
+
+    public bool IsActive
+    {
+      get
+      {
+        DateOnly currentDate = DateOnly.FromDateTime(DateTime.Now);
+        return (currentDate >= StartDate) && (currentDate <= EndDate);
+      }
+    }
+  }
 }
